dceb8cdf0568e9bf06223dbea6b58883370d79d5
Tahir Uzelli <58370244+tahiruzelli@users.noreply.github.com>
2022-01-31 17:42:48 +0300

    project init

---
+++ calculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  calculator
+//
+//  Created by Tahir Uzelli on 31.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
